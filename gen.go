3743f742ed5ac8896415c6c14f6f75ac0c77e482
vinibispo <vini.bispo015@gmail.com>
2024-07-09 11:37:44 -0300

    Add sqlc in our gen.go

--- gen.go
+++ gen.go
@@ -1,3 +1,4 @@
 package main
 
 //go:generate tern migrate --migrations ./internal/pgstore/migrations --config ./internal/pgstore/migrations/tern.conf run go generate ./... | run go generate
+//go:generate sqlc generate -f ./internal/pgstore/sqlc.yaml
